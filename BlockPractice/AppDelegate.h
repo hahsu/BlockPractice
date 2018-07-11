af09486ca4133f718e4f18ee3ca2f044c85c929f
Hannah Hsu <hahsu@fb.com>
2018-07-11 16:56:36 -0700

    Initial Commit

---
+++ BlockPractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlockPractice
+//
+//  Created by Hannah Hsu on 7/11/18.
+//  Copyright © 2018 Hannah Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
